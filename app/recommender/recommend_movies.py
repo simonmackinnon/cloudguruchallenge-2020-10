17bb29939a75b439e2d98828e6c92b29686247e1
Ubuntu <ubuntu@ip-172-31-6-118.ap-southeast-2.compute.internal>
2020-11-04 21:28:15 +0000

    some stuff to do with movie images

--- app/recommender/recommend_movies.py
+++ app/recommender/recommend_movies.py
@@ -3,6 +3,8 @@ import logging
 import os
 import pandas as pd
 import json
+import requests
+from bs4 import BeautifulSoup
 
 from datetime import datetime, timedelta
 from botocore.exceptions import ClientError
@@ -24,8 +26,8 @@ def getTitles(response):
     if not tableIsEmpty(response):
         items = response['Items']
         items.sort(key=lambda x: x['titleid'], reverse=True)
-        df_itmes = pd.DataFrame(items)
-        return df_itmes
+        df_items = pd.DataFrame(items)
+        return df_items
     
     return pd.DataFrame()
 
@@ -42,8 +44,30 @@ def searchForTitles(df_titles, searchString):
 def recommendTitlesForTitleId(df_titles, reftitle):
     logger.info("Recommending for {}".format(reftitle))
     label = df_titles[df_titles['titleid'] == reftitle]['label'].iloc[0]
-    movies = df_titles[df_titles['label'] == label].sample(15)
+    movies = df_titles[df_titles['label'] == label].sample(20)
     return movies
+    
+def getImageUrlForTitle(reftitle, reftitleid):
+    url = 'http://www.imdb.com/title/{}'.format(reftitleid)
+    titleString = '{} Poster'.format(reftitle)
+    response = requests.get(url).content
+    soup = BeautifulSoup(response, 'html.parser')
+    imgs = []
+    for link in soup.find_all('img', title=titleString):
+        imgs.append(link.get('src'))
+    if len(imgs) > 0:
+        logger.info("Img URL for {} is {}".format(reftitle, imgs[0]))
+        return imgs[0]
+    else:
+        logger.info("No Img URL found for {}".format(reftitle))
+        return ""
+
+def getImagesUrlColumn(df_titles):
+    img_urls = []
+    for index, row in df_titles.iterrows():
+        val = getImageUrlForTitle(row['title'], row['titleid'])
+        img_urls.append(val)
+    return img_urls
 
 def main(args, event):
     tableName = args['tableName']
@@ -65,10 +89,25 @@ def main(args, event):
                 return_titles = recommendTitlesForTitleId(df_titles, event["queryStringParameters"]["recommendfortitle"])
             else:
                 logger.error('Invalid query type!')
+                
+        #DON'T ENABLE THIS, (a) T.O.S for IMDB probably forbids scraping of data, and (b), it's reaaaaly slow!
+        # Consider saving the jpg data to the movie data table or s3 for use
+        #df_img_urls = getImagesUrlColumn(return_titles)
+        #return_titles = pd.concat([return_titles, df_img_urls], axis=1, sort=False)
 
         logger.info('Done!')
     except:
         logger.exception('Error in processing!')
+        return { 
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({'Error' : 'Error in processing!'}),
+            'isBase64Encoded': False
+        }
     finally:
         logger.info('Finally Done!')
 
